6f6d454fd1591ab0983eb3a18ea9de26c5e82ad9
AtomMes <atomik.2005@mail.ru>
2022-09-16 22:00:02 +0400

    SearchBar, NavBar,

---
+++ src/utils/constants.js
@@ -0,0 +1,90 @@
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
+import HomeIcon from "@mui/icons-material/Home";
+import CodeIcon from "@mui/icons-material/Code";
+import OndemandVideoIcon from "@mui/icons-material/OndemandVideo";
+import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
+import LiveTvIcon from "@mui/icons-material/LiveTv";
+import SchoolIcon from "@mui/icons-material/School";
+import FemaleIcon from "@mui/icons-material/Female";
+import GraphicEqIcon from "@mui/icons-material/GraphicEq";
+import TheaterComedyIcon from "@mui/icons-material/TheaterComedy";
+import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
+import DeveloperModeIcon from "@mui/icons-material/DeveloperMode";
+
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png'
+
+export const categories = [
+  {
+    name: "all",
+    icon: <HomeIcon />,
+  },
+  {
+    name: "reactjs",
+    icon: <CodeIcon />,
+  },
+  {
+    name: "nextjs",
+    icon: <CodeIcon />,
+  },
+  {
+    name: "music",
+    icon: <MusicNoteIcon />,
+  },
+
+  {
+    name: "films",
+    icon: <OndemandVideoIcon />,
+  },
+  {
+    name: "gaming",
+    icon: <SportsEsportsIcon />,
+  },
+  {
+    name: "live",
+    icon: <LiveTvIcon />,
+  },
+  {
+    name: "learning",
+    icon: <SchoolIcon />,
+  },
+  {
+    name: "sport",
+    icon: <FitnessCenterIcon />,
+  },
+  {
+    name: "fashion",
+    icon: <FemaleIcon />,
+  },
+  {
+    name: "beauty",
+    icon: <FemaleIcon />,
+  },
+  {
+    name: "podcasts",
+    icon: <GraphicEqIcon />,
+  },
+  {
+    name: "comedy",
+    icon: <TheaterComedyIcon />,
+  },
+  {
+    name: "gyms",
+    icon: <FitnessCenterIcon />,
+  },
+  {
+    name: "crypto",
+    icon: <DeveloperModeIcon />,
+  },
+  {
+    name: "coding",
+    icon: <DeveloperModeIcon />,
+  },
+];
+
+
+export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png'
+export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A'
+export const demoVideoUrl = '/video/GDa8kZLNhJ4'
+export const demoChannelTitle = 'JavaScript Mastery'
+export const demoVideoTitle = 'Build and deploy React + Mui project'
+
